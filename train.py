c262e5878346771dbd3d3227f3c160700651adc3
wjnwjn59 <thangduong.no01@gmail.com>
2023-04-16 12:45:12 +0700

    define main

---
+++ train.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa
+import tensorflow as tf
+
+RANDOM_SEED = 59
+tf.random.set_seed(RANDOM_SEED) 
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
